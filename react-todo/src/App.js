5d9d8995eb5af3825e47cee2ab8ac6ecf227196e
dyako-baram <dyako.baram@gmail.com>
2020-10-17 19:43:27 +0300

    half way

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Form from './Components/Form'
+import TodoList from './Components/TodoList'
 import './App.css';
 
 function App() {
+  const [inputText,setInputText]=useState("")
+  const [todos,setTodos]=useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header>
+        <h1>dyako's Todo App</h1>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
